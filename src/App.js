3b436952a12c2403c859fb2dc0e69defe92e3197
jihoon <hun2ya9@gmail.com>
2021-09-28 23:43:42 +0900

    불필요한 파일 제거

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({name, price}){
+  return <div>
+    <h1>I like {name}</h1>
+    <h1>It is {price} \</h1>
+  </div>
+}
+
+const foodIlike = [
+  {key: 1, name: "kimbab", price: 1000},
+  {key: 2, name: "noodle", price: 5000},
+  {key: 3, name: "chicken", price: 15000},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodIlike.map(dish => <Food key={dish.id} name={dish.name} price={dish.price} />
+      )}
     </div>
   );
 }
-
 export default App;
