a9256ab6398404bfa63590182a79ee060f66adc0
jihoon <hun2ya9@gmail.com>
2021-10-02 20:39:01 +0900

    Movie 화면 추가

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Move App
 
 React Study
+
+영화 목록 데이터 출처
+https://yts-proxy.nomadcoders1.now.sh/list_movies.json
--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-function Food({name, price}){
-  return <div>
-    <h1>I like {name}</h1>
-    <h1>It is {price} \</h1>
-  </div>
-}
+import axios from "axios";
+import React from "react";
+import Movie from "./Movie";
+import "./App.css";
 
-const foodIlike = [
-  {key: 1, name: "kimbab", price: 1000},
-  {key: 2, name: "noodle", price: 5000},
-  {key: 3, name: "chicken", price: 15000},
-]
-
-function App() {
-  return (
-    <div>
-      {foodIlike.map(dish => <Food key={dish.id} name={dish.name} price={dish.price} />
-      )}
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 export default App;
